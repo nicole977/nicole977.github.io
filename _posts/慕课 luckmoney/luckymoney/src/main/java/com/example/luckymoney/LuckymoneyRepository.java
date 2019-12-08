de15e0a0bb54f1850d91e55a015563d06d3d422f
陈媛 <670444951@qq.com>
2019-12-08 11:21:47 +0800

    添加博文

--- _posts/慕课 luckmoney/luckymoney/src/main/java/com/example/luckymoney/LuckymoneyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.luckymoney;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//JpaRepository是访问数据库的关键接口；第一个参数是数据库的实体类，第二个参数是id的类型
-public interface LuckymoneyRepository extends JpaRepository<Luckymoney,Integer> {
-
-
-}
